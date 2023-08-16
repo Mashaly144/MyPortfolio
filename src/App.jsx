a5c02c98cb989ca5697828ce9b1a5cb782a1f607
Mashaly144 <117321633+Mashaly144@users.noreply.github.com>
2023-08-16 03:50:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,21 @@ import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <div className='bg-white dark:bg-[#191919]'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/techstack' element={<TechStackPage />} />
-        <Route path='/projects' element={<ProjectsPage />} />
-        <Route path='/contact' element={<ContactsPage />} />
-
-
-      </Routes>
-      <Footer />
+    <div className='dark:bg-[#191919]'>
+      <div className='main  dark:hidden'>
+        <div className='gradient dark:z-0' />
+      </div>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/techstack' element={<TechStackPage />} />
+          <Route path='/projects' element={<ProjectsPage />} />
+          <Route path='/contact' element={<ContactsPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </div>
   );
 }
